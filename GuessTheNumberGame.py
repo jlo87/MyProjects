d2697fdfc3626c65e5d63256198f182848cde2a6
Jonathan Lo <64229972+jlo87@users.noreply.github.com>
2020-06-12 21:34:47 -0400

    Update GuessTheNumberGame.py

--- GuessTheNumberGame.py
+++ GuessTheNumberGame.py
@@ -6,10 +6,10 @@ print('Hello. What is your name?')
 name = input()
 
 print('Well, ' + name + ', I am thinking of a number between 1 and 20.')
-secretNumber = random.randint(1, 20)
+secretNumber = random.randint(1, 20) # Assignment statement
 
 # Ask the player to guess 6 times.
-for guessesTaken in range(1, 7):
+for guessesTaken in range(1, 7): # For loop.
 	print('Take a guess.')
 	guess = int(input()) # Call the input function, store in a variable called 
 	                     # guess, and convert the input to an int.
@@ -23,4 +23,4 @@ for guessesTaken in range(1, 7):
 if guess == secretNumber:
 	print('Good job, ' + name + '! You guessed my number in ' + str(guessesTaken) + ' guesses!')
 else:
-	print('Nope, the number I was thinking of was ' + str(secretNumber))
+	print('Nope, the number I was thinking of was ' + str(secretNumber)) # Convert to str.
